81a16dd4dd5285f1931af06a87a1f2d04d052124
Matt Ronge <mronge@theronge.com>
2009-12-08 14:38:29 -0600

    Build scripts are no longer needed

--- Scripts/BuildScripts/publish.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/sh
-
-# Run this from the root of the MailCore project
-# Arg 1 is the tag name
-
-cp -r Documentation API
-scp -r API mronge.com:/var/www/mronge/m/MailCore/API
-scp build/MailCore.tar.gz mronge.com:/var/www/mronge/m/MailCore/MailCore-$1.tar.gz
