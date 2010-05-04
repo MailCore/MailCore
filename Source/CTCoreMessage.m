29aa63d5d9f8d8589d68f8677cbb922e991c3ccc
Matt Ronge <mronge@theronge.com>
2010-05-03 21:36:29 -0500

    Add support for sending attachments (thanks to Jaremy Creechley)
    --HG--
    branch : KJ-1.0

--- Source/CTCoreMessage.m
+++ Source/CTCoreMessage.m
@@ -37,6 +37,7 @@
 #import "CTMIME_MessagePart.h"
 #import "CTMIME_TextPart.h"
 #import "CTMIME_MultiPart.h"
+#import "CTMIME_SinglePart.h"
 #import "CTBareAttachment.h"
 
 @interface CTCoreMessage (Private)
@@ -208,9 +209,17 @@ char * etpan_encode_mime_header(char * phrase)
 - (void)setBody:(NSString *)body {
 	CTMIME *oldMIME = myParsedMIME;
 	CTMIME_TextPart *text = [CTMIME_TextPart mimeTextPartWithString:body];
-	CTMIME_MessagePart *messagePart = [CTMIME_MessagePart mimeMessagePartWithContent:text];
-	myParsedMIME = [messagePart retain];
-	[oldMIME release];
+	
+	// If myParsedMIME is already a multi-part mime, just add it. otherwise replace it.
+    //TODO: If setBody is called multiple times it will add text parts multiple times. Instead
+    // it should find the existing text part (if there is one) and replace it
+	if ([myParsedMIME isKindOfClass:[CTMIME_MultiPart class]]) {
+		[(CTMIME_MultiPart *)myParsedMIME addMIMEPart:text];
+	} else {
+		CTMIME_MessagePart *messagePart = [CTMIME_MessagePart mimeMessagePartWithContent:text];
+		myParsedMIME = [messagePart retain];
+		[oldMIME release];		
+	}
 }
 
 - (NSArray *)attachments {
@@ -233,6 +242,30 @@ char * etpan_encode_mime_header(char * phrase)
 }
 
 - (void)addAttachment:(CTCoreAttachment *)attachment {
+	CTMIME_MultiPart *multi;
+	CTMIME_MessagePart *msg;
+	
+	if ([myParsedMIME isKindOfClass:[CTMIME_MessagePart class]]) {
+		msg = (CTMIME_MessagePart *)myParsedMIME;
+		CTMIME *sub = [msg content];
+		
+		
+		// Creat new multimime part if needed
+		if ([sub isKindOfClass:[CTMIME_MultiPart class]]) {
+			multi = (CTMIME_MultiPart *)sub;
+		} else {
+			multi = [CTMIME_MultiPart mimeMultiPart];
+			[multi addMIMEPart:sub];
+			[msg setContent:multi];
+		}
+		
+		// add new SinglePart which encodes the attachment in base64
+		CTMIME_SinglePart *attpart = [CTMIME_SinglePart mimeSinglePartWithData:[attachment data]];
+		attpart.contentType = [attachment contentType];
+		attpart.filename = [attachment filename];
+	
+		[multi addMIMEPart:attpart];
+	}
 }
 
 
@@ -408,7 +441,9 @@ char * etpan_encode_mime_header(char * phrase)
 
 
 - (NSString *)render {
-	if ([myParsedMIME isMemberOfClass:[myParsedMIME class]]) {
+	CTMIME *msgPart = myParsedMIME;
+
+	if ([myParsedMIME isKindOfClass:[CTMIME_MessagePart class]]) {
 		/* It's a message part, so let's set it's fields */
 		struct mailimf_fields *fields;
 		struct mailimf_mailbox *sender = (myFields->fld_sender != NULL) ? (myFields->fld_sender->snd_mb) : NULL;
@@ -424,7 +459,7 @@ char * etpan_encode_mime_header(char * phrase)
 		//TODO uh oh, when this get freed it frees stuff in the CTCoreMessage
 		//TODO Need to make sure that fields gets freed somewhere
 		fields = mailimf_fields_new_with_data(from, sender, replyTo, to, cc, bcc, inReplyTo, references, subject);
-		[(CTMIME_MessagePart *)myParsedMIME setIMFFields:fields];
+		[(CTMIME_MessagePart *)msgPart setIMFFields:fields];
 	}
 	return [myParsedMIME render];
 }
